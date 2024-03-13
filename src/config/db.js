b600b2bc87c59f763f8ae85cd0b6151965ec947d
AndreaILara <andreainigolara@hotmail.com>
2024-03-13 10:41:12 +0100

    .env revisado

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URL);
-    console.log('Connected to databas');
+    console.log('Connected to database');
   }
   catch (error) {
     console.log('Error connecting to database');
